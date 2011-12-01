4b18a4b1e93a462de63ea2325c54de8530624395
Pablo Neira Ayuso <pablo@netfilter.org>
2011-12-01 14:51:30 +0100

    update headers to use new nfnetlink subsys

--- include/linux/netfilter/nfnetlink_acct.h
+++ include/linux/netfilter/nfnetlink_acct.h
@@ -2,9 +2,6 @@
 #define _NFNL_ACCT_H_
 #include <linux/netfilter/nfnetlink.h>
 
-/* FIXME: tweak to get it working with external headers. */
-#define NFNL_SUBSYS_ACCT NFNL_SUBSYS_OSF
-
 #define NFACCT_NAME_MAX		64
 
 enum nfnl_acct_msg_types {
@@ -27,8 +24,9 @@ enum nfnl_acct_type {
 
 struct nf_acct;
 
-extern struct nf_acct *nfnl_acct_find_get(char *filter_name);
+extern struct nf_acct *nfnl_acct_find_get(const char *filter_name);
 extern void nfnl_acct_put(struct nf_acct *acct);
+extern void nfnl_acct_update(const struct sk_buff *skb, struct nf_acct *nfacct);
 
 #endif /* __KERNEL__ */
 
